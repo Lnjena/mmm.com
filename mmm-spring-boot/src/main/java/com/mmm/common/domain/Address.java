23b5ae654a8fda20af72e4ae3a23af7be92224ee
arvind.kumar <arvind.kumar@ge.com>
2015-06-23 17:04:03 +0100

    Annotated domain model as per mongoDB

--- mmm-spring-boot/src/main/java/com/mmm/common/domain/Address.java
+++ mmm-spring-boot/src/main/java/com/mmm/common/domain/Address.java
@@ -1,7 +1,16 @@
 package com.mmm.common.domain;
 
-public class Address {
+import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "address")
+public class Address implements Serializable {
+
+	private static final long serialVersionUID = -8485856064060081344L;
+
+	@Id
 	private Long addressId;
 	private String addressDescription;
 	private String addressLine1;
@@ -12,4 +21,75 @@ public class Address {
 	private String country;
 	private String postCode;
 
+	public Long getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+
+	public String getAddressDescription() {
+		return addressDescription;
+	}
+
+	public void setAddressDescription(String addressDescription) {
+		this.addressDescription = addressDescription;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getAddressLine3() {
+		return addressLine3;
+	}
+
+	public void setAddressLine3(String addressLine3) {
+		this.addressLine3 = addressLine3;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPostCode() {
+		return postCode;
+	}
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
 }
--- mmm-spring-boot/src/main/java/com/mmm/partner/domain/Partner.java
+++ mmm-spring-boot/src/main/java/com/mmm/partner/domain/Partner.java
@@ -2,6 +2,9 @@ package com.mmm.partner.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "partner")
 public class Partner implements Serializable {
 
 	private static final long serialVersionUID = 5218143967349070770L;
@@ -12,6 +15,52 @@ public class Partner implements Serializable {
 	private String partnerType;
 	private String partnerRegistrationNumber;
 	private String partnerName;
-	
 
+	public Long getPartnerId() {
+		return partnerId;
+	}
+
+	public void setPartnerId(Long partnerId) {
+		this.partnerId = partnerId;
+	}
+
+	public Long getAddressId() {
+		return addressId;
+	}
+
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
+	}
+
+	public Long getContactId() {
+		return contactId;
+	}
+
+	public void setContactId(Long contactId) {
+		this.contactId = contactId;
+	}
+
+	public String getPartnerType() {
+		return partnerType;
+	}
+
+	public void setPartnerType(String partnerType) {
+		this.partnerType = partnerType;
+	}
+
+	public String getPartnerRegistrationNumber() {
+		return partnerRegistrationNumber;
+	}
+
+	public void setPartnerRegistrationNumber(String partnerRegistrationNumber) {
+		this.partnerRegistrationNumber = partnerRegistrationNumber;
+	}
+
+	public String getPartnerName() {
+		return partnerName;
+	}
+
+	public void setPartnerName(String partnerName) {
+		this.partnerName = partnerName;
+	}
 }
